d16d13572b4cb665e9d4fb145d7c281bc941b5d8
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 20:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+<div id="browser-info"></div>
+
+	<script>
+		// Detect browser name and version using the navigator object
+		let browserName = navigator.appName;
+		let version = navigator.appVersion;
+
+		// Display browser information on the page
+		let browserInfoDiv = document.getElementById("browser-info");
+		browserInfoDiv.innerHTML = "You are using " + browserName + " version " + version;
+	</script>
